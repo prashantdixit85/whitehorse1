1cc76a7703652cbb73116e6bc5dc5aedd7cdde8f
prashant <prashant.dixit@logicwares.com>
2013-10-03 23:58:15 +0530

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def our_work
+  end
+
+  def what_we_do
+  end
+
+  def contact
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whitehorse::Application.config.secret_token = 'a80cf2c86052b658f726e5ab4e70d91057b1e2e43cf9a19ce8d9ee43b68eff60388dfbce8a48f349e2fe68809540c0d6e7781a5aaf2cb25a7f498d53da651d7e'
